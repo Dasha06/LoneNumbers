7704b1c0cc6f05d7ab9b6a4d9c48fc4051b72788
Dasha06 <dasha.nikitina20@yandex.ru>
2024-12-04 17:50:20 +0300

    innit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿class FindOnesNumber 
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Введите числа через запятую для добавления в массив:");
+        string input = Console.ReadLine();
+        int[] numarr;
+        if (!string.IsNullOrEmpty(input))
+        {
+            numarr = input.Split(',').Select(num => int.Parse(num.Trim())).ToArray();
+        }
+        else
+        {
+            numarr = new int[0];
+        }
+
+        int[] result = find(numarr);
+        Console.WriteLine($"[{string.Join(", ", result)}]");
+    }
+    
+    public static int[] find(int[] nums)
+    {
+        Dictionary<int, int> countMap = new Dictionary<int, int>();
+        foreach (int num in nums)
+        {
+            if (countMap.ContainsKey(num))
+            {
+                countMap[num]++;
+            }
+            else
+            {
+                countMap.Add(num, 1);
+            }
+        }
+        
+        List<int> result = new List<int>();
+        foreach (KeyValuePair<int, int> pair in countMap)
+        {
+            if (pair.Value == 1)
+            {
+                result.Add(pair.Key);
+            }
+        }
+        
+        return result.ToArray();
+    }
+}
